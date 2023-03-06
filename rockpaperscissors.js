1d80590753e265b80cc139cbf3aa00ca080e0ff0
Deeshon Hunukumbura <deeshonhunukumbura18@gmail.com>
2023-03-07 01:00:39 +0530

    Edit rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -12,7 +12,17 @@ function getComputerChoice() {
 
 } 
 
-function playRound(playerChoice, computerChoice) {
+let playerScore = 0
+let computerScore = 0
+
+function playRound(choice) {
+
+
+    const pScore = document.querySelector("#pscore")
+    const cScore = document.querySelector("#cscore")
+
+    const h1 = document.querySelector("#msg")
+
 
     const selections = {
         'rock' : 0,
@@ -20,57 +30,32 @@ function playRound(playerChoice, computerChoice) {
         'scissor' : 2
     }
 
+    let playerChoice = choice
     let playerSelection = selections[playerChoice]
+    let computerChoice = getComputerChoice()
     let computerSelection = selections[computerChoice]
 
     switch (playerSelection - computerSelection) {
         case 0:
-            return "Its a Tie!"
+            console.log("Its a Tie!")
+            h1.textContent = "Its a Tie"
+            
+            break
             
         case -1:
         case 2:
-            return `You lose! ${computerChoice} beats ${playerChoice}`
+            console.log(`You lose! ${computerChoice} beats ${playerChoice}`)
+            h1.textContent = `You lose! ${computerChoice} beats ${playerChoice}`
+            computerScore+=1
+            cScore.textContent = `Computer: ${computerScore}`
+            break
             
         case 1:
         case -2:
-            return `You win! ${playerChoice} beats ${computerChoice}`
-    }
-
-}
-
-
-
-function game() {
-    let computerScore = 0
-    let playerScore = 0
-
-    let run = true
-
-    while (run == true) {
-
-        let playerChoice = prompt("Select Rock, Paper or Scissor: ").toLowerCase()
-        let computerChoice = getComputerChoice()
-
-        let result = playRound(playerChoice, computerChoice)
-        console.log(result)
-
-        if (result == `You lose! ${computerChoice} beats ${playerChoice}` ) {
-            computerScore += 1
-            console.log(`Your score: ${playerScore}`)
-            console.log(`Computer's score: ${computerScore}`)
-        } else if (result == `You win! ${playerChoice} beats ${computerChoice}`) {
-            playerScore += 1
-            console.log(`Your score: ${playerScore}`)
-            console.log(`Computer's score: ${computerScore}`)
-        }
-
-        if (playerScore == 5) {
-            console.log("You Win!")
-            run = false
-        } else if (computerScore == 5) {
-            console.log("You lose! Computer Wins")
-            run = false
-        }
+            console.log(`You win! ${playerChoice} beats ${computerChoice}`)
+            h1.textContent = `You win! ${playerChoice} beats ${computerChoice}`
+            playerScore+=1
+            pScore.textContent = `Player: ${playerScore}`
 
     }
 
@@ -78,13 +63,12 @@ function game() {
 }
 
 
-game()
+const buttons = document.querySelectorAll("button")
 
-
-
-
-
-
-
-
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        choice = button.name
+        playRound(choice)
+    })
+})
 
